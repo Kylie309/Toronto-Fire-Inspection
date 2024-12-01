5315784c0200b3b97245e474e0347c9cdeaf6840
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-12-01 11:35:53 -0500

    Updated script for simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,52 +1,61 @@
 #### Preamble ####
-# Purpose: Simulates a dataset of Australian electoral divisions, including the 
-  #state and party that won each division.
-# Author: Rohan Alexander
-# Date: 26 September 2024
-# Contact: rohan.alexander@utoronto.ca
+# Purpose: Simulates a dataset of fire inspection outcomes which contains
+  # violation status by property type and inspection time interval
+# Author: Yunkai Gu
+# Date: 1 December 2024
+# Contact: kylie.gu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: The `tidyverse` package must be installed
-# Any other information needed? Make sure you are in the `starter_folder` rproj
+# Pre-requisites: The `tidyverse` package must be installed.
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-set.seed(853)
+
+
+#### Simulation preparation ####
+# Set seed
+set.seed(123)
+
+# Set number of rows in simulation data
+n <- 13200
+
+# Property types
+property_type <- c(
+  "High Rise",
+  "Low Rise",
+  "Detension",
+  "Group Home",
+  "Group Home (VO)",
+  "Hospital",
+  "Hotel & Motel",
+  "Nursing Home",
+  "Residential Care",
+  "Rooming House"
+)
+
+# Violation indicator
+violation <- c(0,1)
 
 
 #### Simulate data ####
-# State names
-states <- c(
-  "New South Wales",
-  "Victoria",
-  "Queensland",
-  "South Australia",
-  "Western Australia",
-  "Tasmania",
-  "Northern Territory",
-  "Australian Capital Territory"
-)
-
-# Political parties
-parties <- c("Labor", "Liberal", "Greens", "National", "Other")
-
-# Create a dataset by randomly assigning states and parties to divisions
-analysis_data <- tibble(
-  division = paste("Division", 1:151),  # Add "Division" to make it a character
-  state = sample(
-    states,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.25, 0.25, 0.15, 0.1, 0.1, 0.1, 0.025, 0.025) # Rough state population distribution
-  ),
-  party = sample(
-    parties,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.40, 0.40, 0.05, 0.1, 0.05) # Rough party distribution
-  )
+simulated_data <- tibble(
+  #Randomly select from a set of property types
+  property_type = sample(property_type, n, replace = TRUE), 
+  #Randomly selected dates from 2019-07 to 2024-11 as open date
+  inspections_opendate = sample(seq(as.Date("2019-07-01"), 
+                                    as.Date("2024-11-30"), by = "day"), 
+                                n, replace = TRUE),
+  #Randomly selected dates from 2024-01 to 2024-11 as closed date
+  inspections_closeddate = sample(seq(as.Date("2024-01-01"), 
+                                    as.Date("2024-11-30"), by = "day"), 
+                                n, replace = TRUE),
+  # Randomly select integers from 0 to 1700 
+  date_num = sample(0:1700, n, replace = TRUE),
+  #Randomly select from 1 and 0 for violation indicator
+  violation = sample(violation, n, replace = TRUE)
 )
 
 
 #### Save data ####
-write_csv(analysis_data, "data/00-simulated_data/simulated_data.csv")
+write_csv(simulated_data, "data/00-simulated_data/fire_simulated_data.csv")
