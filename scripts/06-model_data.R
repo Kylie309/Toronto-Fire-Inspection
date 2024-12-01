35804378d9633b717923a1c35494bef73176aa36
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-12-01 10:57:41 -0500

    Updated script for modeling

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Construct Bayesian Model for fire inspection outcome
 # Author: Yunkai Gu
-# Date: 24 November 2024
+# Date: 28 November 2024
 # Contact: kylie.gu@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Downloaded and cleaned data.
@@ -10,40 +10,41 @@
 
 #### Workspace setup ####
 library(tidyverse)
-library(janitor)
 library(lubridate)
-library(broom)
-library(modelsummary)
 library(rstanarm)
-library(splines)
+
 
 #### Read data ####
 cleaned_data <- read_csv("data/02-analysis_data/fire_cleaned_data.csv")
 
 
 ### Model data ####
+# Change 'property_type' to factor variables
 cleaned_data <- cleaned_data |>
   mutate(
-    address_name = factor(address_name),
     property_type = factor(property_type)
   )
 
+# Specify priors
 priors <- normal(0, 2.5, autoscale = TRUE)
 
+# Fit Bayesian model with date_num and property_type as fixed effect
 bayesian_model_1 <- stan_glm(
   formula = violation ~ date_num + property_type,
   data = cleaned_data,
   family = binomial(link = "logit"),
-  prior = priors,
-  prior_intercept = priors,
+  prior = normal(0, 2.5, autoscale = TRUE),
+  prior_intercept = normal(0, 2.5, autoscale = TRUE),
   seed = 123,
   cores = 4,
   adapt_delta = 0.95,
-  iter = 2000
+  iter = 4000
 )
 
+
+### Save the model ####
 saveRDS(
   bayesian_model_1,
-  file = "models/model_1.rds"
+  file = "models/violation_prediction_model.rds"
 )
 
