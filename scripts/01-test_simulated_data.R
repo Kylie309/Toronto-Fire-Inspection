21302b4d9409ee973db8d298d289b12f3048f794
Kylie309 <kylie.gu@mail.utoronto.ca>
2024-12-01 12:11:48 -0500

    Updated script for testing the simulated dataset

--- scripts/01-test_simulated_data.R
+++ scripts/01-test_simulated_data.R
@@ -1,20 +1,25 @@
 #### Preamble ####
-# Purpose: Tests the structure and validity of the simulated Australian 
-  #electoral divisions dataset.
-# Author: Rohan Alexander
-# Date: 26 September 2024
-# Contact: rohan.alexander@utoronto.ca
+# Purpose: Tests the structure and validity of the simulated fire inspection
+  #outcome dataset.
+# Author: Yunkai Gu
+# Date: 1 December 2024
+# Contact: kylie.gu@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
   # - The `tidyverse` package must be installed and loaded
   # - 00-simulate_data.R must have been run
-# Any other information needed? Make sure you are in the `starter_folder` rproj
+# Any other information needed? Make sure you are in the `Toronto_Fire_Inspection` rproj
 
 
 #### Workspace setup ####
 library(tidyverse)
 
-analysis_data <- read_csv("data/00-simulated_data/simulated_data.csv")
+
+# Read the dataset
+analysis_data <- read_csv("data/00-simulated_data/fire_simulated_data.csv")
+
+
+#### Test data ####
 
 # Test if the data was successfully loaded
 if (exists("analysis_data")) {
@@ -23,48 +28,72 @@ if (exists("analysis_data")) {
   stop("Test Failed: The dataset could not be loaded.")
 }
 
-
-#### Test data ####
-
-# Check if the dataset has 151 rows
-if (nrow(analysis_data) == 151) {
-  message("Test Passed: The dataset has 151 rows.")
+# Check if the dataset has 13193 rows
+if (nrow(analysis_data) == 13193) {
+  message("Test Passed: The dataset has 13193 rows.")
 } else {
-  stop("Test Failed: The dataset does not have 151 rows.")
+  stop("Test Failed: The dataset does not have 13193 rows.")
 }
 
-# Check if the dataset has 3 columns
-if (ncol(analysis_data) == 3) {
-  message("Test Passed: The dataset has 3 columns.")
+# Check if the dataset has 5 columns
+if (ncol(analysis_data) == 5) {
+  message("Test Passed: The dataset has 5 columns.")
 } else {
-  stop("Test Failed: The dataset does not have 3 columns.")
+  stop("Test Failed: The dataset does not have 5 columns.")
 }
 
-# Check if all values in the 'division' column are unique
-if (n_distinct(analysis_data$division) == nrow(analysis_data)) {
-  message("Test Passed: All values in 'division' are unique.")
+# Check if the 'property_type' column contains only valid property types
+valid_types <- c("High Rise",
+                  "Low Rise",
+                  "Detension",
+                  "Group Home",
+                  "Group Home (VO)",
+                  "Hospital",
+                  "Hotel & Motel",
+                  "Nursing Home",
+                  "Residential Care",
+                  "Rooming House")
+
+if (all(analysis_data$property_type %in% valid_types)) {
+  message("Test Passed: The 'property_type' column contains only valid property types.")
 } else {
-  stop("Test Failed: The 'division' column contains duplicate values.")
+  stop("Test Failed: The 'property_type' column contains invalid property types.")
 }
 
-# Check if the 'state' column contains only valid Australian state names
-valid_states <- c("New South Wales", "Victoria", "Queensland", "South Australia", 
-                  "Western Australia", "Tasmania", "Northern Territory", 
-                  "Australian Capital Territory")
+# Check if the 'inspections_opendate' column contains only valid dates
+valid_opendate <- seq(as.Date("2019-07-09"), as.Date("2024-11-20"), by = "day")
 
-if (all(analysis_data$state %in% valid_states)) {
-  message("Test Passed: The 'state' column contains only valid Australian state names.")
+if (all(analysis_data$inspections_opendate %in% valid_opendate)) {
+  message("Test Passed: The 'inspections_opendate' column contains only valid open dates.")
 } else {
-  stop("Test Failed: The 'state' column contains invalid state names.")
+  stop("Test Failed: The 'inspections_opendate' column contains invalid open dates.")
 }
 
-# Check if the 'party' column contains only valid party names
-valid_parties <- c("Labor", "Liberal", "Greens", "National", "Other")
+# Check if the 'inspections_closeddate' column contains only valid dates
+valid_closeddate <- seq(as.Date("2024-01-02"), as.Date("2024-11-28"), by = "day")
 
-if (all(analysis_data$party %in% valid_parties)) {
-  message("Test Passed: The 'party' column contains only valid party names.")
+if (all(analysis_data$inspections_closeddate %in% valid_closeddate)) {
+  message("Test Passed: The 'inspections_closeddate' column contains only valid closed dates.")
 } else {
-  stop("Test Failed: The 'party' column contains invalid party names.")
+  stop("Test Failed: The 'inspections_closeddate' column contains invalid closed dates.")
+}
+
+# Check if the 'date_num' column contains only valid numbers
+valid_date <- c(0:1679)
+
+if (all(analysis_data$date_num %in% valid_date)) {
+  message("Test Passed: The 'date_num' column contains only valid numbers.")
+} else {
+  stop("Test Failed: The 'date_num' column contains invalid numbers.")
+}
+
+# Check if the 'violation' column contains only 0 and 1
+valid_violation <- c(0,1)
+
+if (all(analysis_data$violation %in% valid_violation)) {
+  message("Test Passed: The 'violation' column contains only 0 and 1.")
+} else {
+  stop("Test Failed: The 'violation' column contains intergers other than 0 and 1.")
 }
 
 # Check if there are any missing values in the dataset
@@ -74,16 +103,9 @@ if (all(!is.na(analysis_data))) {
   stop("Test Failed: The dataset contains missing values.")
 }
 
-# Check if there are no empty strings in 'division', 'state', and 'party' columns
-if (all(analysis_data$division != "" & analysis_data$state != "" & analysis_data$party != "")) {
-  message("Test Passed: There are no empty strings in 'division', 'state', or 'party'.")
+# Check if there are no empty strings in 'property_type' column
+if (all(analysis_data$property_type != "")) {
+  message("Test Passed: There are no empty strings in 'property_type' column.")
 } else {
-  stop("Test Failed: There are empty strings in one or more columns.")
-}
-
-# Check if the 'party' column has at least two unique values
-if (n_distinct(analysis_data$party) >= 2) {
-  message("Test Passed: The 'party' column contains at least two unique values.")
-} else {
-  stop("Test Failed: The 'party' column contains less than two unique values.")
+  stop("Test Failed: There are empty strings in 'property_type' column.")
 }
